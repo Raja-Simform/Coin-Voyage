846a9f903f58971064524fe6dae5518fe0dd80ba
Raja-Simform <raja.jha@simformsolutions.com>
2025-03-31 16:46:05 +0530

    feat:add add score method

--- src/controller/contoller.ts
+++ src/controller/contoller.ts
@@ -1,17 +1,34 @@
+type Player = {
+    count: number,
+    posX: number,
+    posY: number
+}
 export class Contoller{
     private arrSize:number;
     constructor(arrSize:number){
        this.arrSize=arrSize;
-       console.log(this.init(arrSize));
-     }
-     init(arrSize:number):number[][]{
+       const value=this.init(arrSize);
+       console.log(value);
+       const player:Player={
+         count:5,
+         posX: 1,
+         posY:2
+       }
+       const temp=this.addScore(value,player);
+       console.log(`the score is ${temp.player.count} & ${temp.player.posX} ${temp.player.posY}`)
+           
+    }
+    init(arrSize:number){
         const arr:number[][]=[]; 
+        // const gameOver=0;
         for (let a=0;a<arrSize;a++){
                 arr[a]=[];
+                
                 for (let b=0;b<arrSize;b++){
                     const sum=Math.floor(Math.random()*10);
                     if(sum===0){
                         arr[a][b]=0;
+                        // gameOver++;
                     }
                     else if(sum%2===0){
                         arr[a][b]=3;
@@ -22,5 +39,13 @@ export class Contoller{
                 }
         }
         return arr;
-     }
+    }
+   
+    addScore(arr:number[][], player: Player){
+           player.count+=arr[player.posX][player.posY];
+           arr[player.posX][player.posY]=0;
+           return {arr,player};
+    }
+    
+
 }
